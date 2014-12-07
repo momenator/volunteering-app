dc7238843d1852ad1395cb45c072823b58fde84a
Muhammad Rafdi <muhammad@Muhammads-MacBook-Pro.local>
2014-12-07 03:28:23 +0000

    updated organisation entity

--- src/main/java/se/group6/vfa/entity/Organisation.java
+++ src/main/java/se/group6/vfa/entity/Organisation.java
@@ -1,18 +1,30 @@
 package se.group6.vfa.entity;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Organisation {
 	@Id
 	@GeneratedValue
 	private Integer organisation_id;
+
 	private String organisation_name;
+
 	private String email;
+
 	private String description;
+
 	private String address;
+
+	@OneToMany
+	@JoinTable
+	private List<VW> VW_available;
 
 	public Integer getOrganisation_id() {
 		return organisation_id;
