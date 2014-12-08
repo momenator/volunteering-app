fcc2a6364691c79ec7dd6049bcdd969f8e1f6a1f
Muhammad Rafdi <muhammad@Muhammads-MacBook-Pro.local>
2014-12-08 17:05:26 +0000

    changed security config and added login controller

---
+++ src/main/java/se/group6/vfa/controller/LoginController.java
@@ -0,0 +1,13 @@
+package se.group6.vfa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+}
