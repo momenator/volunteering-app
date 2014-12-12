a883adb00893580cda523287f09a9531a07a84a0
mombi93 <m.d.a.rafdi@gmail.com>
2014-12-12 10:13:22 +0000

    changed the comment on test class and final commit

--- src/test/java/se/group6/vfa/service/VWServiceTest.java
+++ src/test/java/se/group6/vfa/service/VWServiceTest.java
@@ -1,47 +1,66 @@
-/*
- * package se.group6.vfa.service;
- * 
- * import static org.mockito.Mockito.mock; import junit.framework.Assert;
- * 
- * import org.junit.After; import org.junit.Before; import org.junit.Test;
- * import org.junit.runner.RunWith; import
- * org.springframework.beans.factory.annotation.Autowired; import
- * org.springframework.test.context.ContextConfiguration; import
- * org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
- * 
- * import se.group6.vfa.entity.VW; import se.group6.vfa.repository.VWRepository;
- * 
- * @RunWith(SpringJUnit4ClassRunner.class)
- * 
- * @ContextConfiguration(locations = {
- * "file:src/main/webapp/WEB-INF/applicationContext.xml" }) public class
- * VWServiceTest {
- * 
- * private VWService vwService;
- * 
- * private VW vw;
- * 
- * @Autowired private VWRepository vwRepository;
- * 
- * @Before public void setUp() throws Exception { vwService =
- * mock(VWService.class); vwRepository = mock(VWRepository.class); vw =
- * mock(VW.class); }
- * 
- * @After public void tearDown() throws Exception { vwService = null; }
- * 
- * @Test public void checkIfvwServiceIsNullReturnsFalse() {
- * Assert.assertNotNull(vwService.findAll()); }
- * 
- * @Test public void testFindAllReturnsAllListOfVW() {
- * Assert.assertEquals(vwService.findAll(), vwRepository.findAll()); }
- * 
- * @Test public void testFindOneVWReturnsOneVWWithSameID() { VW vwTest = new
- * VW(); vwTest.setId(20); vwRepository.save(vwTest);
- * Assert.assertEquals(vwService.findOne(20), vwRepository.findOne(20)); }
- * 
- * @Test public void testSaveVWExpectSuccessfullySavedVW() { VW vwTest2 = new
- * VW(); vwTest2.setId(19); vwService.save(vwTest2);
- * Assert.assertEquals(vwService.findOne(19), vwRepository.findOne(19)); }
- * 
- * }
- */
+//package se.group6.vfa.service;
+//
+//import static org.mockito.Mockito.mock;
+//import junit.framework.Assert;
+//
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import se.group6.vfa.entity.VW;
+//import se.group6.vfa.repository.VWRepository;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/applicationContext.xml" })
+//public class VWServiceTest {
+//
+//	private VWService vwService;
+//
+//	private VW vw;
+//
+//	@Autowired
+//	private VWRepository vwRepository;
+//
+//	@Before
+//	public void setUp() throws Exception {
+//		vwService = mock(VWService.class);
+//		vwRepository = mock(VWRepository.class);
+//		vw = mock(VW.class);
+//	}
+//
+//	@After
+//	public void tearDown() throws Exception {
+//		vwService = null;
+//	}
+//
+//	@Test
+//	public void checkIfvwServiceIsNullReturnsFalse() {
+//		Assert.assertNotNull(vwService.findAll());
+//	}
+//
+//	@Test
+//	public void testFindAllReturnsAllListOfVW() {
+//		Assert.assertEquals(vwService.findAll(), vwRepository.findAll());
+//	}
+//
+//	@Test
+//	public void testFindOneVWReturnsOneVWWithSameID() {
+//		VW vwTest = new VW();
+//		vwTest.setId(20);
+//		vwRepository.save(vwTest);
+//		Assert.assertEquals(vwService.findOne(20), vwRepository.findOne(20));
+//	}
+//
+//	@Test
+//	public void testSaveVWExpectSuccessfullySavedVW() {
+//		VW vwTest2 = new VW();
+//		vwTest2.setId(19);
+//		vwService.save(vwTest2);
+//		Assert.assertEquals(vwService.findOne(19), vwRepository.findOne(19));
+//	}
+//
+// }
